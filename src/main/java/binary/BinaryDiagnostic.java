9f91fef693c4fe41e3c9c3656be34f1a38a40b4e
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2021-12-03 13:16:29 +0100

    solution of part one

--- src/main/java/binary/BinaryDiagnostic.java
+++ src/main/java/binary/BinaryDiagnostic.java
@@ -1,5 +1,63 @@
 package binary;
 
+import java.util.List;
+
 public class BinaryDiagnostic {
+
+    private static final String EXAMPLE_ENTRY = "111011101000";
+
+    public static long calculatePartOne(List<String> diagnosticReport) {
+        long gammaRate = Long.parseLong(calculateGammaRate(diagnosticReport), 2);
+        long epsilonRate = Long.parseLong(calculateEpsilonRate(diagnosticReport), 2);
+        return gammaRate * epsilonRate;
+    }
+
+    private static String calculateGammaRate(List<String> diagnosticReport) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < EXAMPLE_ENTRY.length(); i++) {
+            int zeroCount = 0;
+            int oneCount = 0;
+            for(String entry : diagnosticReport) {
+                if(entry.charAt(i) == '0') {
+                    zeroCount++;
+                } else {
+                    oneCount++;
+                }
+            }
+
+            if(oneCount > zeroCount) {
+                sb.append("1");
+            } else {
+                sb.append("0");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String calculateEpsilonRate(List<String> diagnosticReport) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < EXAMPLE_ENTRY.length(); i++) {
+            int zeroCount = 0;
+            int oneCount = 0;
+            for(String entry : diagnosticReport) {
+                if(entry.charAt(i) == '0') {
+                    zeroCount++;
+                } else {
+                    oneCount++;
+                }
+            }
+
+            if(oneCount > zeroCount) {
+                sb.append("0");
+            } else {
+                sb.append("1");
+            }
+        }
+
+        return sb.toString();
+    }
 
 }
--- src/test/java/binary/BinaryDiagnosticTest.java
+++ src/test/java/binary/BinaryDiagnosticTest.java
@@ -1,5 +1,36 @@
 package binary;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
 public class BinaryDiagnosticTest {
+
+
+    private static final String INPUT_FILE_PATH_1 = "src/test/resources/binary-diagnostic/input1.txt";
+    private final List<String> diagnosticReport = new ArrayList<>();
+
+    @BeforeAll
+    public void setup() throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(INPUT_FILE_PATH_1))) {
+            diagnosticReport.addAll(lines.collect(Collectors.toList()));
+        }
+    }
+
+    @Test
+    public void testPartOne() {
+        long correctAnswer = 3813416;
+        Assertions.assertEquals(correctAnswer, BinaryDiagnostic.calculatePartOne(diagnosticReport), "Wrong answer");
+    }
 
 }
