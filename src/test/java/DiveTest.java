a712fe26f95f91399d2b7609dd9b4ba7c46cab4b
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2021-12-03 11:37:43 +0100

    introduce Dive

---
+++ src/test/java/DiveTest.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class DiveTest {
+
+    private static final String INPUT_FILE_PATH_1 = "src/test/resources/dive/input1.txt";
+    private List<String> input1;
+
+    @BeforeAll
+    public void setup() throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(INPUT_FILE_PATH_1))) {
+            input1 = lines.collect(Collectors.toList());
+        }
+    }
+
+    @Test
+    public void testFirstScenario() {
+
+    }
+}
