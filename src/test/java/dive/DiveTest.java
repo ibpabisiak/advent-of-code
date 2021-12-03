aa148221848e05baf976ac606d3580a8d58fbcb0
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2021-12-03 12:06:40 +0100

    refactor

--- src/test/java/dive/DiveTest.java
+++ src/test/java/dive/DiveTest.java
@@ -1,3 +1,5 @@
+package dive;
+
 import dive.DiveCommand;
 import java.io.IOException;
 import java.nio.file.Files;
--- src/test/java/sonar/SonarSweepTest.java
+++ src/test/java/sonar/SonarSweepTest.java
@@ -1,3 +1,5 @@
+package sonar;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
