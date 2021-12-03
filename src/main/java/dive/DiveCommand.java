788893129f2129c6cda3e741b5f9dbaf15a34726
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2021-12-03 12:17:32 +0100

    minor update

--- src/main/java/dive/DiveCommand.java
+++ src/main/java/dive/DiveCommand.java
@@ -2,13 +2,11 @@ package dive;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class DiveCommand {
 
     private String command;
